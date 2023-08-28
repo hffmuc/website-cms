87f1f2b560adad8160110b048e9e4cec2c07eaab
AILab-HFF <KILabHFF@hotmail.com>
2023-08-28 17:19:29 +0200

    adapted cors policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  // 'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      headers: '*',
+      origin: ["http://localhost:1337", "http://10.10.0.215:1337"]
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
