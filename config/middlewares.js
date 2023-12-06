d197b29dedb30551df85ec567a20b5d7ab794577
ki <ki@vfxrender17.hffpostpro.de>
2023-12-06 14:20:45 +0100

    added port 1338 to middleware.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       headers: '*',
-      origin: ["http://localhost:1337", "http://localhost:1337/", "http://localhost:3000", "http://kilab.hff-muc.de", "https://kilab.hff-muc.de", "http://128.140.103.202", "https://128.140.103.202", "http://127.0.0.1:1337", "http://128.140.103.202:1337"]
+      origin: ["http://localhost:1337", "http://localhost:1337/", "http://localhost:1338", "http://localhost:1338/", "http://localhost:3000", "http://kilab.hff-muc.de", "https://kilab.hff-muc.de", "http://128.140.103.202", "https://128.140.103.202", "http://127.0.0.1:1337", "http://128.140.103.202:1337"]
     }
   },
   'strapi::logger',
